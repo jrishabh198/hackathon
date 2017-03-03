26d211e91f264846e9affff64794f991625cb1e5
Rishabh <jrishabh198@gmail.com>
2017-03-03 13:53:40 +0530

    added wifi hotspot and mac address identification

--- Socius/app/src/main/java/com/example/rj/socius/MainActivity.java
+++ Socius/app/src/main/java/com/example/rj/socius/MainActivity.java
@@ -1,13 +1,270 @@
 package com.example.rj.socius;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.reflect.Method;
+import java.net.NetworkInterface;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button enableButton,disableButton,hotspotButton;
+    WifiManager mWifiManager;
+    ListView listView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String p = getMacAddr();
+        Log.v("abcd","efgh");
+        Log.v("h",p);
+        enableButton =(Button)findViewById(R.id.button);
+        disableButton = (Button)findViewById(R.id.button2);
+        hotspotButton= (Button)findViewById(R.id.button1);
+        enableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+//                wifi.setWifiEnabled(true);
+                getConnectedDevicesMac();
+            }
+        });
+
+        disableButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                wifi.setWifiEnabled(false);
+            }
+        });
+
+        hotspotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                if(wifiManager.isWifiEnabled())
+                {
+                    wifiManager.setWifiEnabled(false);
+                }
+
+                WifiConfiguration netConfig = new WifiConfiguration();
+
+                netConfig.SSID = "MyAP";
+                netConfig.preSharedKey = "1234567890";
+                netConfig.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+                netConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+                netConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+                netConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+
+                try{
+                    Method setWifiApMethod = wifiManager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+                    boolean apstatus=(Boolean) setWifiApMethod.invoke(wifiManager, netConfig,true);
+
+                    Method isWifiApEnabledmethod = wifiManager.getClass().getMethod("isWifiApEnabled");
+                    while(!(Boolean)isWifiApEnabledmethod.invoke(wifiManager)){};
+                    Method getWifiApStateMethod = wifiManager.getClass().getMethod("getWifiApState");
+                    int apstate=(Integer)getWifiApStateMethod.invoke(wifiManager);
+                    Method getWifiApConfigurationMethod = wifiManager.getClass().getMethod("getWifiApConfiguration");
+                    netConfig=(WifiConfiguration)getWifiApConfigurationMethod.invoke(wifiManager);
+                    Log.e("CLIENT", "\nSSID:"+netConfig.SSID+"\nPassword:"+netConfig.preSharedKey+"\n");
+                    getConnectedDevicesMac();
+
+                } catch (Exception e) {
+                    Log.e(this.getClass().toString(), "", e);
+            }
+        }});
+        mWifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        registerReceiver(mWifiScanReceiver,
+                new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        mWifiManager.startScan();
+
+
+
+    }
+
+    public static ArrayList<String> getConnectedDevicesMac()
+    {
+        ArrayList<String> res = new ArrayList<String>();
+        //NetManager.updateArpFile();
+
+        BufferedReader br;
+        try
+        {
+            br = new BufferedReader(new FileReader("/proc/net/arp"));
+            String line;
+            line = br.readLine();
+            while ((line = br.readLine()) != null)
+            {
+                String[] sp = line.split(" +");
+                if (sp[3].matches("..:..:..:..:..:.."))
+                    res.add(sp[3]);
+            }
+
+            br.close();
+        }
+        catch (Exception e)
+        {}
+        Log.v("testing","test");
+        for(int i =0;i<res.size();i++){
+            Log.v("testing",res.get(i));
+        }
+        return res;
+    }
+    public static String getMacAddr() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(Integer.toHexString(b & 0xFF) + ":");
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                Log.v("jklm","nopq");
+                Log.v("Mac",res1.toString());
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+        }
+        return "02:00:00:00:00:00";
+    }
+    private final BroadcastReceiver mWifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent intent) {
+            if (intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                List<ScanResult> mScanResults = mWifiManager.getScanResults();
+                String[] values = new String[mScanResults.size()];
+                for(int i =0;i<mScanResults.size();i++){
+                    values[i]= mScanResults.get(i).SSID;
+                }
+
+                ArrayAdapter adapter = new ArrayAdapter<String>(getApplicationContext(),
+                        R.layout.activity_listview, values);
+                // add your logic
+                listView = (ListView) findViewById(R.id.list);
+                listView.setAdapter(adapter);
+                connectWiFi(mScanResults.get(0));
+
+
+            }
+        }
+    };
+
+    public void connectWiFi(ScanResult scanResult) {
+        try {
+
+            Log.v("rht", "Item clicked, SSID " + scanResult.SSID + " Security : " + scanResult.capabilities);
+
+            String networkSSID = scanResult.SSID;
+            Toast.makeText(getApplicationContext(),networkSSID,Toast.LENGTH_LONG).show();
+
+            String networkPass = "fullstop";
+
+            WifiConfiguration conf = new WifiConfiguration();
+            conf.SSID = "\"" + networkSSID + "\"";   // Please note the quotes. String should contain ssid in quotes
+            conf.status = WifiConfiguration.Status.ENABLED;
+            conf.priority = 40;
+
+            if (scanResult.capabilities.toUpperCase().contains("WEP")) {
+                Log.v("rht", "Configuring WEP");
+                conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+                conf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+                conf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.SHARED);
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+
+                if (networkPass.matches("^[0-9a-fA-F]+$")) {
+                    conf.wepKeys[0] = networkPass;
+                } else {
+                    conf.wepKeys[0] = "\"".concat(networkPass).concat("\"");
+                }
+
+                conf.wepTxKeyIndex = 0;
+
+            } else if (scanResult.capabilities.toUpperCase().contains("WPA")) {
+                Log.v("rht", "Configuring WPA");
+
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+                conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+
+                conf.preSharedKey = "\"" + networkPass + "\"";
+
+            } else {
+                Log.v("rht", "Configuring OPEN network");
+                conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+                conf.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+                conf.allowedAuthAlgorithms.clear();
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+                conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            }
+
+            WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            int networkId = wifiManager.addNetwork(conf);
+
+            Log.v("rht", "Add result " + networkId);
+
+            List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+            for (WifiConfiguration i : list) {
+                if (i.SSID != null && i.SSID.equals("\"" + networkSSID + "\"")) {
+                    Log.v("rht", "WifiConfiguration SSID " + i.SSID);
+
+                    boolean isDisconnected = wifiManager.disconnect();
+                    Log.v("rht", "isDisconnected : " + isDisconnected);
+
+                    boolean isEnabled = wifiManager.enableNetwork(i.networkId, true);
+                    Log.v("rht", "isEnabled : " + isEnabled);
+
+                    boolean isReconnected = wifiManager.reconnect();
+                    Log.v("rht", "isReconnected : " + isReconnected);
+
+                    break;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
